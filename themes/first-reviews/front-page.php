238ca9943e7c79585a1b4b60242d5859fe21c4f6
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-01-17 12:37:16 -0500

    replaced hero section with featured review

--- themes/first-reviews/front-page.php
+++ themes/first-reviews/front-page.php
@@ -3,10 +3,14 @@
 <?php get_header(); ?>
 
 <div class="container">
-  <section class="hero">
-    <img src="<?php the_field( 'hero_banner' ); ?>" />
-    <h1><?php the_field( 'hero_heading' ); ?></h1>
-    <?php the_field( 'hero_paragraph' ); ?>
+  <section class="featured">
+    <?php
+    $featured_review = get_field('featured_review');
+    if( $featured_review ): ?>
+      <h3><?php echo esc_html( $featured_review->post_title ); ?></h3>
+      <h3><?php echo ( $featured_review->post_content ); ?></h3>
+      <h3><?php echo ( $featured_review->rating ); ?></h3>
+    <?php endif; ?>
   </section>
 </div>
 
