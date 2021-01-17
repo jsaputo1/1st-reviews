89c682856da0796aaeeaa10225183c294fefcbc1
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-01-17 17:26:47 -0500

    initial code for featured review

--- themes/first-reviews/front-page.php
+++ themes/first-reviews/front-page.php
@@ -7,9 +7,45 @@
     <?php
     $featured_review = get_field('featured_review');
     if( $featured_review ): ?>
-      <h3><?php echo esc_html( $featured_review->post_title ); ?></h3>
-      <h3><?php echo ( $featured_review->post_content ); ?></h3>
-      <h3><?php echo ( $featured_review->rating ); ?></h3>
+      <?php
+      $args = array( 
+          'p' => get_field('featured_review')->ID
+          );
+      $product_posts = get_posts( $args ); 
+      ?>
+      <?php foreach ( $product_posts as $post ) : setup_postdata( $post ); ?>  
+      <div class="featured-container">
+        <div class="featured-image">
+          <img src="<?php echo get_the_post_thumbnail_url();?>">
+          <div class="text">
+            <h5><?php echo the_field( 'rating' ); ?></h5>
+            <?php $genre = '';
+              $value = get_field('genre');
+              if ($value) {
+                $genre = implode(' <span class="delimiter"> | </span>', $value);
+              }
+            ?>
+            <h3><?php echo $genre; ?></h3>
+            <h3><?php echo the_field( 'year' ); ?></h2>
+        </div>
+        </div> 
+        <div class="featured-text">
+          <div class="featured-title">
+            <h1><?php echo the_title(); ?></h1>
+            <div class="rating">
+              <?php echo the_field( 'rating' ); ?>
+            </div>
+          </div>
+          <?php echo wp_trim_words( get_the_content(), 130, '...' ) ?>
+          <a href="<?php echo get_the_permalink() ?>" class="btn primary">Read More</a>
+          <h4><em><?php echo get_the_author() ?></em>
+          <span> <?php echo get_the_date(); ?></span></h4>
+          <div class="tags">
+            <?php echo get_the_tag_list('',', ','')?>
+          </div>
+        </div>
+      </div>
+      <?php endforeach; wp_reset_postdata(); ?>
     <?php endif; ?>
   </section>
 </div>
--- themes/first-reviews/functions.php
+++ themes/first-reviews/functions.php
@@ -4,6 +4,7 @@
 function first_reviews_files() {
     wp_enqueue_style('first_reviews_styles', get_stylesheet_uri('/build/css/style.min.css'), NULL, microtime());
     wp_enqueue_style('fonts', "https://fonts.googleapis.com/css?family=Lato&display=swap");
+    wp_enqueue_script('load-fa', 'https://kit.fontawesome.com/e785bdc78c.js');
 }
 
 add_action('wp_enqueue_scripts', 'first_reviews_files');
