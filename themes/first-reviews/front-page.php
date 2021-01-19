07b9b37b386fb5b94e3cc5a91b2c5c2ecc4b36f2
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-01-19 12:56:14 -0500

    styled featured section

--- themes/first-reviews/front-page.php
+++ themes/first-reviews/front-page.php
@@ -2,7 +2,6 @@
 <!-- Header  -->
 <?php get_header(); ?>
 
-<div class="container">
   <section class="featured">
     <?php
     $featured_review = get_field('featured_review');
@@ -18,6 +17,7 @@
         <div class="featured-image">
           <img src="<?php echo get_the_post_thumbnail_url();?>">
           <div class="text">
+            <h2><?php echo the_field( 'movie_title' ); ?></h2>
             <h5><?php echo the_field( 'rating' ); ?></h5>
             <?php $genre = '';
               $value = get_field('genre');
@@ -36,51 +36,55 @@
               <?php echo the_field( 'rating' ); ?>
             </div>
           </div>
-          <p><?php echo wp_trim_words( get_the_content(), 130, '...' ) ?></p>
-          <a href="<?php echo get_the_permalink() ?>" class="btn primary">Read More</a>
-          <h4><em><?php echo get_the_author() ?></em>
-          <span> <?php echo get_the_date(); ?></span></h4>
-          <div class="tags">
-            <?php echo get_the_tag_list('',', ','')?>
+          <p>
+          <?php echo wp_trim_words( get_the_content(), 75, '...' ) ?>
+          <span> <a href="<?php echo get_the_permalink() ?>" class="trunc-link">Read More</a></span>
+          </p>
+          <div class="featured-post-info">
+            <h4><em><?php the_author_link() ?></em>
+            <span> <?php echo get_the_date(); ?></span></h4>
           </div>
         </div>
       </div>
       <?php endforeach; wp_reset_postdata(); ?>
     <?php endif; ?>
   </section>
-  <section class="latest-reviews">
-    <h1>Latest</h1>
-  <?php
-      $args = array( 
-          'post_type' => 'post', 
-          'order' => 'ASC',
-          'cat' => '7'
-          );
-      $review_posts = get_posts( $args ); 
-  ?>
-  <?php foreach ( $review_posts as $post ) : setup_postdata( $post ); ?> 
-  <div class="post-container">
-    <div class="thumbnail">
-      <img src="<?php echo get_the_post_thumbnail_url();?>">
+  <div class="container">
+    <section class="latest-reviews">
+      <h1>Latest</h1>
+    <?php
+        $args = array( 
+            'post_type' => 'post', 
+            'order' => 'ASC',
+            'cat' => '7'
+            );
+        $review_posts = get_posts( $args ); 
+    ?>
+    <?php foreach ( $review_posts as $post ) : setup_postdata( $post ); ?> 
+    <div class="post-container">
+      <div class="thumbnail">
+        <img src="<?php echo get_the_post_thumbnail_url();?>">
+      </div>
+      <div class="post-text">
+        <h1><?php echo the_title(); ?></h1>
+        <h5><?php echo the_field( 'rating' ); ?></h5>
+        <p>
+        <?php echo wp_trim_words( get_the_content(), 40, '...' ) ?>
+        <span> <a href="<?php echo get_the_permalink() ?>">Read More</a></span>
+        </p>
+        <h4><em><?php echo get_the_author() ?></em>
+        <span> <?php echo get_the_date(); ?></span></h4>
+      </div>
     </div>
-    <div class="post-text">
-      <h1><?php echo the_title(); ?></h1>
-      <h5><?php echo the_field( 'rating' ); ?></h5>
-      <p><?php echo wp_trim_words( get_the_content(), 40, '...' ) ?></p>
-      <a href="<?php echo get_the_permalink() ?>" class="btn primary">Read More</a>
-      <h4><em><?php echo get_the_author() ?></em>
-      <span> <?php echo get_the_date(); ?></span></h4>
-    </div>
+    <hr>
+
+
+
+
+    <?php endforeach; wp_reset_postdata(); ?>
+    </section>
+
   </div>
-  <hr>
-
-
-
-
-  <?php endforeach; wp_reset_postdata(); ?>
-  </section>
-
-</div>
 
 <!-- Footer -->
 <?php get_footer();?>
