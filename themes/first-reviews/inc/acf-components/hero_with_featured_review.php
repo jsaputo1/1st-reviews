cd2f1ad41cb3f576a34dd56ad87bf4e69e015ea1
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-05-01 17:56:12 -0400

    restructed header and hero component

--- themes/first-reviews/inc/acf-components/hero_with_featured_review.php
+++ themes/first-reviews/inc/acf-components/hero_with_featured_review.php
@@ -9,34 +9,25 @@ $featured_post = get_sub_field( 'featured_post');
 
 if( $featured_post ): ?>
 
-  <section class="hero-with-featured-review">
-    <?php foreach( $featured_post as $post ): ?> 
-      <div class="featured-container row">
-        <div class="featured-image col-12">
+
+  <section class="hero-with-featured-review row">
+    <?php foreach( $featured_post as $post ): ?>
+        <div class="featured-image col-lg-7">
           <img src="<?php echo get_the_post_thumbnail_url();?>">
-          <div class="black-block"></div>
           <div class="text">
-            <h3><?php echo the_sub_field( 'movie_title' ); ?>
-            <h5><?php echo the_sub_field( 'rating' ); ?></h5>
+            <h3><?php echo get_field( 'movie_title', $post->ID ); ?></h3>
+            <span><?php echo get_field( 'rating', $post->ID ); ?></span>
           </div>
         </div> 
-        <div class="featured-text col-6">
-          <div class="featured-title">
-            <h1><?php echo the_title(); ?></h1>
-            <div class="rating">
-              <?php echo the_sub_field( 'rating' ); ?>
-            </div>
-          </div>
-          <p>
-          <?php echo wp_trim_words( get_the_content(), 90, '...' ) ?>
-          <span> <a href="<?php echo get_the_permalink() ?>" class="trunc-link">Read More</a></span>
-          </p>
-          <div class="featured-post-info">
+        <div class="featured-text col-lg-5">
+            <h2><?php echo get_the_title(); ?></h2>
+            <span class="rating"><?php echo get_field( 'rating', $post->ID ); ?></span>
+          <p><?php echo wp_trim_words( get_the_content(), 90, '...' ) ?><span><a href="<?php echo get_the_permalink() ?>" class="trunc-link">Read More</a></span></p>
+          <div class="footer">
             <h4><em><?php the_author_link() ?></em>
             <span> <?php echo get_the_date(); ?></span></h4>
           </div>
         </div>
-      </div>
       <?php endforeach; ; ?>
     <?php endif; ?>
   </section>
--- themes/first-reviews/page.php
+++ themes/first-reviews/page.php
@@ -15,48 +15,40 @@
 get_header();
 ?>
 
-<div class="main-split">
 
-    <main id="primary" class="site-main col-10-lg">
+<main id="primary" class="site-main col-12">
 
-    <?php
-    // ID of the current item in the WordPress Loop
-    $id = get_the_ID();
+<?php
+// ID of the current item in the WordPress Loop
+$id = get_the_ID();
 
-    // check if the flexible content field has rows of data
-    if ( have_rows( 'components', $id ) ) :
+// check if the flexible content field has rows of data
+if ( have_rows( 'components', $id ) ) :
 
-        ?>
-
-        <?php
-
-        // loop through the selected ACF layouts and display the matching partial
-        while ( have_rows( 'components', $id ) ) : the_row();
-
-            get_template_part( 'inc/acf-components/' . get_row_layout() );
-
-        endwhile;
-
-    elseif ( get_the_content() ) :
-
-        ?>
-
-
-        <h1>no layouts found</h1>
-
-        <?php
-    endif;
     ?>
 
-    </main><!-- #main -->
+    <?php
 
-    <section class="sidebar col-lg-2">
-        <?php
-        get_sidebar();
-        ?>
-    </section>
+    // loop through the selected ACF layouts and display the matching partial
+    while ( have_rows( 'components', $id ) ) : the_row();
 
-</div>
+        get_template_part( 'inc/acf-components/' . get_row_layout() );
+
+    endwhile;
+
+elseif ( get_the_content() ) :
+
+    ?>
+
+
+    <h1>no layouts found</h1>
+
+    <?php
+endif;
+?>
+
+</main><!-- #main -->
+
 
 <?php
 
