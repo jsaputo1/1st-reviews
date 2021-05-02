c8f20a788b4b6da99ec11c77f31d4d8139b3a464
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-05-02 14:13:01 -0400

    restructed latest reviews to acf

--- themes/first-reviews/inc/acf-components/hero_with_featured_review.php
+++ themes/first-reviews/inc/acf-components/hero_with_featured_review.php
@@ -9,8 +9,8 @@ $featured_post = get_sub_field( 'featured_post');
 
 if( $featured_post ): ?>
 
-
-  <section class="hero-with-featured-review row">
+  <section class="hero-with-featured-review row span-page">
+    <h1><?php echo get_sub_field( 'heading' ); ?></h1>
     <?php foreach( $featured_post as $post ): ?>
         <div class="featured-image col-lg-7">
           <img src="<?php echo get_the_post_thumbnail_url();?>">
@@ -20,7 +20,7 @@ if( $featured_post ): ?>
           </div>
         </div> 
         <div class="featured-text col-lg-5">
-            <h2><?php echo get_the_title(); ?></h2>
+            <h2><a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title(); ?></a></h2>
             <span class="rating"><?php echo get_field( 'rating', $post->ID ); ?></span>
           <p><?php echo wp_trim_words( get_the_content(), 90, '...' ) ?><span><a href="<?php echo get_the_permalink() ?>" class="trunc-link">Read More</a></span></p>
           <div class="footer">
--- themes/first-reviews/inc/acf-components/latest_reviews.php
+++ themes/first-reviews/inc/acf-components/latest_reviews.php
@@ -1,30 +1,36 @@
-<section class="latest-reviews">
-  <h1 class="main-title">Latest</h1>
-  <?php
-      $args = array( 
-          'post_type' => 'post', 
-          'order' => 'ASC',
-          'cat' => '7'
-          );
-      $review_posts = get_posts( $args ); 
-  ?>
-  <?php foreach ( $review_posts as $post ) : setup_postdata( $post ); ?> 
-    <div class="post-container">
-      <div class="thumbnail">
-        <img src="<?php echo get_the_post_thumbnail_url();?>">
+<?php 
+/**
+* Latest Reviews Component
+*/
+?> 
+ 
+ <section class="latest-reviews col-lg-10">
+    <h2 class="title">Latest</h2>
+    <?php
+        $args = array( 
+            'post_type' => 'post', 
+            'order' => 'ASC',
+            'cat' => '7'
+            );
+        $review_posts = get_posts( $args ); 
+    ?>
+    <?php foreach ( $review_posts as $post ) : setup_postdata( $post ); ?> 
+      <div class="row post-wrapper">
+        <div class="col-lg-4 thumbnail">
+          <img src="<?php echo get_the_post_thumbnail_url();?>">
+        </div>
+        <div class="text col-lg-8">
+          <h2><a href="<?php echo get_the_permalink() ?>"><?php echo the_title(); ?></a></h2>
+          <span class="rating"><?php echo the_field( 'rating' ); ?></span>
+          <p>
+          <?php echo wp_trim_words( get_the_content(), 65, '...' ) ?>
+          <span> <a href="<?php echo get_the_permalink() ?>">Read More</a></span>
+          </p>
+          <h4><strong>By <?php echo get_the_author() ?></strong>
+          <span> <?php echo get_the_date(); ?></span></h4>
+        </div>
       </div>
-      <div class="post-text">
-        <h1><?php echo the_title(); ?></h1>
-        <h5><?php echo the_field( 'rating' ); ?></h5>
-        <p>
-        <?php echo wp_trim_words( get_the_content(), 30, '...' ) ?>
-        <span> <a href="<?php echo get_the_permalink() ?>">Read More</a></span>
-        </p>
-        <h4><strong>By <?php echo get_the_author() ?></strong>
-        <span> <?php echo get_the_date(); ?></span></h4>
-      </div>
-    </div>
-    <hr>
+      <hr>
 
-  <?php endforeach; wp_reset_postdata(); ?>
-</section>
+    <?php endforeach; wp_reset_postdata(); ?>
+  </section>
