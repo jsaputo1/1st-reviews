afa3338f4aa86425d87009850d6edbfc81beef50
jsaputo1 <59458399+jsaputo1@users.noreply.github.com>
2021-07-14 21:48:50 -0400

    added acf fields. added link to header, adjusted margins

--- themes/first-reviews/inc/acf-components/hero_with_featured_review.php
+++ themes/first-reviews/inc/acf-components/hero_with_featured_review.php
@@ -15,14 +15,14 @@ if( $featured_post ): ?>
         <div class="featured-image col-lg-7">
           <img src="<?php echo get_the_post_thumbnail_url();?>">
           <div class="text">
-            <h3><?php echo get_field( 'movie_title', $post->ID ); ?></h3>
+            <a href="<?php echo get_the_permalink(); ?>"><?php echo get_field( 'movie_title', $post->ID ); ?></a>
             <span><?php echo get_field( 'rating', $post->ID ); ?></span>
           </div>
         </div> 
         <div class="featured-text col-lg-5">
-            <h2><a href="<?php echo get_the_permalink() ?>"><?php echo get_the_title(); ?></a></h2>
+            <h2><a href="<?php echo get_the_permalink() ?>"><?php echo get_field( 'movie_title', $post->ID ); ?></a></h2>
             <span class="rating"><?php echo get_field( 'rating', $post->ID ); ?></span>
-          <p><?php echo wp_trim_words( get_the_content(), 90, '...' ) ?><span><a href="<?php echo get_the_permalink() ?>" class="trunc-link">Read More</a></span></p>
+          <p><?php echo wp_trim_words( get_the_content(), 90, '...' ) ?><span><a href="<?php echo get_the_permalink(); ?>" class="trunc-link">Read More</a></span></p>
           <div class="footer">
             <?php
               $author = get_the_author_meta('display_name', 1); 
